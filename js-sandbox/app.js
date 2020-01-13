bdf96501476db49cf58e2e578b68f3aa45d44862
robihamdani <robi4us@gmail.com>
2020-01-13 08:36:29 +0700

    first commit

---
+++ js-sandbox/app.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
